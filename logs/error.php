fb3c887e4268ef252d1ee4fac047e0917f8d7311
redbobrob <redbobrob@gmail.com>
2016-03-25 01:01:12 -0700

    joomla
    3.0.14 VM
    3.5.0 JM

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-03-25 06:37:45 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-03-25T06:37:45+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-03-25T06:38:21+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
